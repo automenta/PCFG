734170f2be3837d69f351c35667708128cda3a09
koormoosh <koormoosh@gmail.com>
2014-04-30 13:09:49 +1000

    Fixed bugs in HyperParamInferencer
    Added check_c, check_d, invalidLog functions.
    - check_c and check_d are put to handle some special
    cases where c becomes too large or d becomes 0 or 1.

--- src/pcfg/ViterbiParsing.java
+++ src/pcfg/ViterbiParsing.java
@@ -18,7 +18,7 @@ public class ViterbiParsing {
 
     public static void main(String[] args) throws FileNotFoundException, URISyntaxException {
         File file = new File(Thread.currentThread().getContextClassLoader().getResource("").toURI());
-        Scanner sc = new Scanner(new File(file.getParentFile().getParent() + "\\resource\\cnf.txt"));
+        Scanner sc = new Scanner(new File(file.getParentFile().getParent() + "\\resource\\training.txt"));
         while (sc.hasNextLine()) {
             String sentence = "";
             Node root = new Node("S");
